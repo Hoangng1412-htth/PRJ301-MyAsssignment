64b0cd7310bc52a670217b3c8e048e1a6a1b1a27
Nguyễn Hoàng <hoangcdt10@gmail.com>
2025-11-02 23:54:37 +0700

    Create Division.java

---
+++ MyProjectAssignment/src/java/model/Division.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ng hoang
+ */
+public class Division {
+    private String dname;
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+    
+}
