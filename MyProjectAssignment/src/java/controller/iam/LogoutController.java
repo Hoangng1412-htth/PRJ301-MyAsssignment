7e297649b432386b270801ca4a1bc25e5c397458
Nguyễn Hoàng <hoangcdt10@gmail.com>
2025-11-05 04:29:20 +0700

    Create LogoutController.java

---
+++ MyProjectAssignment/src/java/controller/iam/LogoutController.java
@@ -0,0 +1,28 @@
+package controller.iam;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // Hủy session đăng nhập
+        req.getSession().invalidate();
+        // Quay lại trang login
+        resp.sendRedirect(req.getContextPath() + "/login");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
